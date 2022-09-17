8d7227e6413c8ebdba85b83d45d58fbc991a7f74
suppanut.sittimakarn <arisomna1878@gmail.com>
2022-09-17 20:17:50 +0700

    add axios and abort function

--- src/redux/saga/dogSaga.ts
+++ src/redux/saga/dogSaga.ts
@@ -1,9 +1,24 @@
+import axios from "axios";
 import { call, put, takeEvery } from "redux-saga/effects";
 import { getDogSuccess } from "../slice/shibaReducer";
 
 function* workGetDogsFetch(): any {
-  const dogs = yield call(() => fetch("https://api.thedogapi.com/v1/breeds"));
-  const formattedDogs = yield dogs.json();
+  const controller = new AbortController();
+  const dogs = yield call(() =>
+    axios
+      .get("https://api.thedogapi.com/v1/breeds", { signal: controller.signal })
+      .then((value) => {
+        return value;
+      })
+      .catch((err) => {
+        if (axios.isCancel(err)) {
+          controller.abort();
+        } else {
+          // TODO: handle error
+        }
+      })
+  );
+  const formattedDogs = yield JSON.parse(JSON.stringify(dogs.data));
   const formattedDogsShortened = formattedDogs
     .filter((value: any) => value.name === "Shiba Inu")
     .concat(formattedDogs.slice(0, 10));
